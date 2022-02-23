d69c00a70382f8f9ae06ad18cd1585c0041a5a1c
rorosi <56014938+rorosi@users.noreply.github.com>
2022-02-23 21:08:36 +0900

    Add files via upload

---
+++ Chapter_07/ex33.cpp/time2.cpp
@@ -0,0 +1,47 @@
+#include "time2.h"
+
+Time::Time()
+{
+	hours = mins = 0;
+}
+
+Time::Time(int h, int m) {
+	hours = h;
+	mins = m;
+}
+
+void Time::addHours(int h) {
+	hours += h;
+};
+
+void Time::addMins(int m) {
+	mins += m;
+	hours += mins / 60;
+	mins %= 60;
+};
+
+Time Time::operator+(Time& t) {
+	Time sum;
+	sum.mins = mins + t.mins;
+	sum.hours = hours + t.hours;
+	sum.hours += sum.mins / 60;
+	sum.mins %= 60;
+	return sum;
+};
+
+void Time::show() {
+	std::cout << "½Ã°£ : " << hours << std::endl;
+	std::cout << "ºĞ : " << mins << std::endl;
+}
+
+Time::~Time()
+{
+}
+
+Time Time::operator*(int n) {
+	Time result;
+	long resultMin = hours * n * 60 + mins * n;
+	result.hours = resultMin / 60;
+	result.mins = resultMin % 60;
+	return result;
+}
---
+++ Chapter_07/ex33.cpp/time2.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#ifndef TIMEH
+#define TIMEH
+
+class Time
+{
+private:
+	int hours;
+	int mins;
+
+public:
+	Time();
+	Time(int, int);
+	void addHours(int);
+	void addMins(int);
+	Time operator+(Time&);
+	Time operator*(int);
+	void show();
+	~Time();
+	friend Time operator*(int n, Time& t) {
+		return t * n;
+	}
+};
+
+#endif //TIMEH
